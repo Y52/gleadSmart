84382c60c4d89fd828bfd527cc4cf1d7041da42a
Yu Shengzheng <y52gemini@gmail.com>
2018-11-16 10:33:48 +0800

    师弟要做的

---
+++ gleadSmart/Main/MyHouse/SelectDeviceTypeController.m
@@ -0,0 +1,29 @@
+//
+//  SelectDeviceTypeController.m
+//  gleadSmart
+//
+//  Created by 杭州轨物科技有限公司 on 2018/11/16.
+//  Copyright © 2018年 杭州轨物科技有限公司. All rights reserved.
+//
+
+#import "SelectDeviceTypeController.h"
+
+@interface SelectDeviceTypeController ()
+
+#warning TODO 完成设备类型选择UI
+@property (strong, nonatomic) UITableView *deviceTypeTable;
+
+@end
+
+@implementation SelectDeviceTypeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma mark - Lazy Load
+
+#pragma mark - UITableView delegate&datasource
+
+@end
