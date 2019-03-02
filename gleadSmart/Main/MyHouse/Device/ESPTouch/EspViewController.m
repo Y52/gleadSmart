735bc1168dc1f5397870f03ae38f671578977e48
Yu Shengzheng <y52gemini@gmail.com>
2019-03-02 18:06:12 +0800

    完成了新的配网页面UI，待完成功能

--- gleadSmart/Main/MyHouse/Device/ESPTouch/EspViewController.m
+++ gleadSmart/Main/MyHouse/Device/ESPTouch/EspViewController.m
@@ -17,6 +17,8 @@
 
 @property (nonatomic, strong) UIView *backgroundView;
 @property (nonatomic, strong) UITextField *passwordTF;
+@property (nonatomic, strong) UILabel *wifiLabel;
+@property (nonatomic, strong) UIButton *changeWifiButton;
 @property (nonatomic, strong) UIButton *nextBtn;
 
 @end
@@ -30,6 +32,9 @@
     self.navigationItem.title = LocalString(@"添加设备");
     self.backgroundView = [self backgroundView];
     self.passwordTF = [self passwordTF];
+    self.wifiLabel = [self wifiLabel];
+    self.changeWifiButton = [self changeWifiButton];
+    self.nextBtn = [self nextBtn];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -44,7 +49,7 @@
         _backgroundView.backgroundColor = [UIColor whiteColor];
         [self.view addSubview:_backgroundView];
         [_backgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.size.mas_equalTo(CGSizeMake(yAutoFit(320.f), 200.f));
+            make.size.mas_equalTo(CGSizeMake(yAutoFit(320.f), 210.f));
             make.top.equalTo(self.view.mas_top).offset(80.f);
             make.centerX.equalTo(self.view.mas_centerX);
         }];
@@ -54,6 +59,7 @@
         UILabel *titleLabel = [[UILabel alloc] init];
         titleLabel.text = LocalString(@"输入家中Wi-Fi密码");
         titleLabel.font = [UIFont systemFontOfSize:17.f];
+        titleLabel.adjustsFontSizeToFitWidth = YES;
         [_backgroundView addSubview:titleLabel];
         [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
             make.size.mas_equalTo(CGSizeMake(120.f, 20.f));
@@ -71,43 +77,93 @@
         [self.backgroundView addSubview:passwordView];
         [passwordView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.size.mas_equalTo(CGSizeMake(yAutoFit(260.f), 50.f));
-            make.top.equalTo(self.backgroundView.mas_bottom).offset(60.f);
+            make.top.equalTo(self.backgroundView.mas_top).offset(60.f);
             make.centerX.equalTo(self.backgroundView.mas_centerX);
         }];
 
         UIImageView *imageView = [[UIImageView alloc] init];
         imageView.image = [UIImage imageNamed:@"img_wifipwd"];
         [passwordView addSubview:imageView];
-        [passwordView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.size.mas_equalTo(CGSizeMake(18.f, 18.f));
+        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(14.f, 18.f));
             make.left.equalTo(passwordView.mas_left).offset(20.f);
             make.centerY.equalTo(passwordView.mas_centerY);
         }];
         
         _passwordTF = [[UITextField alloc] init];
         _passwordTF.textColor = [UIColor colorWithHexString:@"A1A1A1"];
+        _passwordTF.placeholder = LocalString(@"请输入Wi-Fi密码");
         [passwordView addSubview:_passwordTF];
         [_passwordTF mas_makeConstraints:^(MASConstraintMaker *make) {
             make.size.mas_equalTo(CGSizeMake(yAutoFit(180.f), 20.f));
             make.centerY.equalTo(passwordView.mas_centerY);
-            make.left.equalTo(imageView.mas_right);
+            make.left.equalTo(imageView.mas_right).offset(15.f);
         }];
         
     }
     return _passwordTF;
 }
 
+- (UILabel *)wifiLabel{
+    if (!_wifiLabel) {
+        _wifiLabel = [[UILabel alloc] init];
+        _wifiLabel.font = [UIFont systemFontOfSize:13.f];
+        _wifiLabel.textColor = [UIColor colorWithRed:150/255.0 green:150/255.0 blue:150/255.0 alpha:1.0];
+        _wifiLabel.text = @"Wi-Fi：CMCC-602";
+        _wifiLabel.adjustsFontSizeToFitWidth = YES;
+        [self.backgroundView addSubview:_wifiLabel];
+        [_wifiLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(200.f, 30.f));
+            make.left.equalTo(self.backgroundView.mas_left).offset(yAutoFit(30.f));
+            make.top.equalTo(self.backgroundView.mas_top).offset(120.f);
+        }];
+    }
+    return _wifiLabel;
+}
+
+- (UIButton *)changeWifiButton{
+    if (!_changeWifiButton) {
+        _changeWifiButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_changeWifiButton setTitleColor:[UIColor colorWithRed:57/255.0 green:135/255.0 blue:248/255.0 alpha:1.0] forState:UIControlStateNormal];
+        [_changeWifiButton setTitle:LocalString(@"更改网络") forState:UIControlStateNormal];
+        [_changeWifiButton.titleLabel setFont:[UIFont fontWithName:@"PingFangSC-Medium" size:13]];
+        _changeWifiButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+        [_changeWifiButton addTarget:self action:@selector(changeWifi) forControlEvents:UIControlEventTouchUpInside];
+        [self.backgroundView addSubview:_changeWifiButton];
+        
+        [_changeWifiButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(80.f, 30.f));
+            make.centerY.equalTo(self.wifiLabel.mas_centerY);
+            make.right.equalTo(self.backgroundView.mas_right).offset(yAutoFit(-30.f));
+        }];
+
+    }
+    return _changeWifiButton;
+}
+
 - (UIButton *)nextBtn{
     if (!_nextBtn) {
+        UIView *lineView = [[UIView alloc] init];
+        lineView.backgroundColor = [UIColor lightGrayColor];
+        [self.backgroundView addSubview:lineView];
+        [lineView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.height.mas_equalTo(1.f);
+            make.top.equalTo(self.wifiLabel.mas_bottom).offset(10.f);
+            make.left.equalTo(self.backgroundView.mas_left);
+            make.right.equalTo(self.backgroundView.mas_right);
+        }];
+        
         self.nextBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_nextBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [_nextBtn setTitle:LocalString(@"下一步") forState:UIControlStateNormal];
-        [_nextBtn.titleLabel setFont:[UIFont fontWithName:@"PingFangSC-Medium" size:16]];
-        [_nextBtn setBackgroundColor:[UIColor colorWithRed:71/255.0 green:120/255.0 blue:204/255.0 alpha:0.4]];
-        [_nextBtn setButtonStyle1];
-        //_nextBtn.enabled = NO;
+        [_nextBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_nextBtn setTitle:LocalString(@"确定") forState:UIControlStateNormal];
+        [_nextBtn.titleLabel setFont:[UIFont fontWithName:@"PingFangSC-Medium" size:20]];
         [_nextBtn addTarget:self action:@selector(goNextView) forControlEvents:UIControlEventTouchUpInside];
-        [self.view addSubview:_nextBtn];
+        [self.backgroundView addSubview:_nextBtn];
+        [_nextBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(yAutoFit(320.f), 50.f));
+            make.top.equalTo(lineView.mas_bottom);
+            make.centerX.equalTo(self.backgroundView.mas_centerX);
+        }];
     }
     return _nextBtn;
 }
