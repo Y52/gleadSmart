3cae10e174d5c4b9d40bd0b7e5dcb5a12b40cb06
安建伟 <anjianwei@anjianweideMac-mini.local>
2019-03-11 16:52:19 +0800

    修改修改密码UI合并cell

--- gleadSmart/Cell/Login/PhoneVerifyCell.h
+++ gleadSmart/Cell/Login/PhoneVerifyCell.h
@@ -1,12 +1,15 @@
 //
 //  PhoneVerifyCell.h
-//  Heating
+//  gleadSmart
 //
-//  Created by Mac on 2018/11/12.
-//  Copyright © 2018 Mac. All rights reserved.
+//  Created by 安建伟 on 2019/3/11.
+//  Copyright © 2019 杭州轨物科技有限公司. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
 typedef void(^TFBlock)(NSString *text);
 typedef BOOL(^BtnBlock)(void);
 
@@ -17,9 +20,10 @@ typedef BOOL(^BtnBlock)(void);
 @property (nonatomic, strong) UIImageView *verifyimage;
 @property (nonatomic) TFBlock TFBlock;
 @property (nonatomic) BtnBlock BtnBlock;
-
 @property (strong, nonatomic) dispatch_source_t timer;
 
 -(void)openCountdown;
 
 @end
+
+NS_ASSUME_NONNULL_END
