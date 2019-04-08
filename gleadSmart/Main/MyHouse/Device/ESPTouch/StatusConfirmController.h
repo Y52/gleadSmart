9a41c49548c5f2ace2169da721506f47d71422f6
Yu Shengzheng <y52gemini@gmail.com>
2019-04-08 17:19:48 +0800

    status

---
+++ gleadSmart/Main/MyHouse/Device/ESPTouch/StatusConfirmController.h
@@ -0,0 +1,17 @@
+//
+//  StatusConfirmController.h
+//  gleadSmart
+//
+//  Created by 杭州轨物科技有限公司 on 2019/4/8.
+//  Copyright © 2019年 杭州轨物科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StatusConfirmController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ gleadSmart/Main/MyHouse/Device/ESPTouch/StatusConfirmController.m
@@ -0,0 +1,32 @@
+//
+//  StatusConfirmController.m
+//  gleadSmart
+//
+//  Created by 杭州轨物科技有限公司 on 2019/4/8.
+//  Copyright © 2019年 杭州轨物科技有限公司. All rights reserved.
+//
+
+#import "StatusConfirmController.h"
+
+@interface StatusConfirmController ()
+
+@end
+
+@implementation StatusConfirmController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
