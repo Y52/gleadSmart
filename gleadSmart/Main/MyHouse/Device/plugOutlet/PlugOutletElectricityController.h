5457a77f81c3959e3ea4d1638f9d9178245249a7
安建伟 <anjianwei@anjianweideMac-mini.local>
2019-05-22 20:06:10 +0800

    新增电量UI

---
+++ gleadSmart/Main/MyHouse/Device/plugOutlet/PlugOutletElectricityController.h
@@ -0,0 +1,18 @@
+//
+//  PlugOutletElectricityController.h
+//  gleadSmart
+//
+//  Created by 安建伟 on 2019/5/22.
+//  Copyright © 2019 杭州轨物科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlugOutletElectricityController : UIViewController
+@property (nonatomic, strong) DeviceModel *device;
+
+@end
+
+NS_ASSUME_NONNULL_END
